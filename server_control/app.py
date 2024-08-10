dfb1feb4b789bcfa07fa219ed6ffdc05a6a17b8f
Noah P <noah.pa@yahoo.com>
2024-08-10 12:04:14 +0100

    First working version!

--- server_control/app.py
+++ server_control/app.py
@@ -1,11 +1,48 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, Response
 import subprocess
+import threading
+import time
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'
+app.secret_key = '906dce92e75e3427ce2052e49d0a0091'
 
 # In-memory database for servers
 servers = []
+
+def fetch_sensor_data(server_id):
+    server = servers[server_id]
+    method = server['method']
+    ip = server['ip']
+    username = server['username']
+    password = server['password']
+    
+    if method == 'ipmi':
+        command = f"./servercontrol.sh ipmi sensor_data -i {ip} -u {username} -p {password}"
+    elif method == 'snmp':
+        community = server.get('community')
+        oid = server.get('oid')
+        version = server.get('version')
+        command = f"./servercontrol.sh snmp sensor_data -a {ip} -c {community} -s {oid} -v {version} -d"
+    elif method == 'redfish':
+        command = f"./servercontrol.sh redfish sensor_data -r {ip} -u {username} -p {password} -d"
+    
+    try:
+        result = subprocess.check_output(command, shell=True).decode('utf-8').strip()
+        return result
+    except subprocess.CalledProcessError:
+        return "Failed to retrieve sensor data"
+
+def generate_sensor_data():
+    while True:
+        for idx in range(len(servers)):
+            data = fetch_sensor_data(idx)
+            # Escape special characters
+            escaped_data = data.replace('\n', '\\n').replace('"', '\\"')
+            # Format the data as JSON
+            yield f"data: {{\"server_id\": {idx}, \"data\": \"{escaped_data}\"}}\n\n"
+            time.sleep(10)
+
+
 
 @app.route('/')
 def index():
@@ -24,7 +61,8 @@ def add_server():
         'method': method,
         'ip': ip,
         'username': username,
-        'password': password
+        'password': password,
+        'sensor_data': ''
     }
     servers.append(server)
     flash(f'Server {name} added successfully!')
@@ -58,6 +96,10 @@ def control(server_id, action):
         flash(f'Failed to execute {action} on server {server["name"]}.', 'error')
     
     return redirect(url_for('index'))
+
+@app.route('/events')
+def events():
+    return Response(generate_sensor_data(), content_type='text/event-stream')
 
 if __name__ == '__main__':
     app.run(debug=True)
